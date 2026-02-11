fc8c2448eb13116538ab71d5f2737489fcc27146
Mounika P <pmounika382008@gmail.com>
2026-02-11 09:04:24 +0530

    Update program4f.c

--- program4f.c
+++ program4f.c
@@ -15,4 +15,7 @@ int n, sum = 0;
     printf("Sum of the first %d natural numbers is: %d\n", n, sum);
 
     return 0; 
+
 }
+output:Enter a positive integer: 5
+    Sum of the first 5 natural numbers is: 15
