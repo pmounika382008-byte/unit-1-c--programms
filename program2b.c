ebaa20901bbba19599bc4e8faf819224e9b4dbaa
Mounika P <pmounika382008@gmail.com>
2026-02-11 08:43:28 +0530

    Update program2b.c

--- program2b.c
+++ program2b.c
@@ -25,4 +25,72 @@ int main() {
     printf("String: %s\n", string_value);
 
     return 0;
+
 }
+output://C program for Input and Display Various Data Types
+#include <stdio.h>
+
+int main() {
+    int integer_value;
+    float float_value;
+    double double_value;
+    char character_value;
+    char string_value[50];
+    printf("Enter an integer: ");
+    scanf("%d", &integer_value);
+    printf("Enter a floating-point number (float): ");
+    scanf("%f", &float_value);
+     printf("Enter a double-precision floating-point number (double): ");
+    scanf("%lf", &double_value);
+    printf("Enter a character: ");
+    scanf(" %c", &character_value);
+     printf("Enter a string: ");
+    scanf("%s", string_value);
+     printf("\n--- Displaying Entered Values ---\n");
+    printf("Integer: %d\n", integer_value);
+    printf("Float: %f\n", float_value);
+    printf("Double: %lf\n", double_value);
+    printf("Character: %c\n", character_value);
+    printf("String: %s\n", string_value);
+
+    return 0;
+}
+output://C program for Input and Display Various Data Types
+#include <stdio.h>
+
+int main() {
+    int integer_value;
+    float float_value;
+    double double_value;
+    char character_value;
+    char string_value[50];
+    printf("Enter an integer: ");
+    scanf("%d", &integer_value);
+    printf("Enter a floating-point number (float): ");
+    scanf("%f", &float_value);
+     printf("Enter a double-precision floating-point number (double): ");
+    scanf("%lf", &double_value);
+    printf("Enter a character: ");
+    scanf(" %c", &character_value);
+     printf("Enter a string: ");
+    scanf("%s", string_value);
+     printf("\n--- Displaying Entered Values ---\n");
+    printf("Integer: %d\n", integer_value);
+    printf("Float: %f\n", float_value);
+    printf("Double: %lf\n", double_value);
+    printf("Character: %c\n", character_value);
+    printf("String: %s\n", string_value);
+
+    return 0;
+}
+output:Enter an integer: 25
+Enter a floating-point number (float): 3.14
+Enter a double-precision floating-point number (double): 123.456
+Enter a character: A
+Enter a string: Hello
+    Integer: 25
+Float: 3.140000
+Double: 123.456000
+Character: A
+String: Hello
+    
